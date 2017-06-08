108593622a89bbe1da7d1fcb444240883d096d76
Cristian Sulea <cristian.sulea.79@gmail.com>
2017-06-08 12:21:27 +0300

    junit

--- src/test/java/jatoo/cli/image/JatooCLICommandTest.java
+++ src/test/java/jatoo/cli/image/JatooCLICommandTest.java
@@ -16,66 +16,103 @@
 
 package jatoo.cli.image;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import jatoo.image.ImageUtils;
+
 public class JatooCLICommandTest {
 
-  public static void main(String[] args) {
-    
-//    new JatooCLI().execute(
-//        new String[] {
-//            "-image",
-//            "-resize",
-//            "-fill",
-//            "-widthx", "800",
-//            "-height", "800",
-//            "-removeMetadata",
-//            "-overwrite",
-//            "-in", "src/test/resources/jatoo/cli/image/20141109144518.jpg",
-//            "-out", "target/tests"
-//        });
-//    
-//    System.out.println();
-//    System.out.println("====================================================================================================");
-//    System.out.println();
-//    
-//    new JatooCLI().execute(
-//        new String[] {
-//            "-image",
-//            "-resize",
-//            "-fit",
-//            "-width", "800",
-//            "-height", "800",
-//            "-removeMetadata",
-//            "-overwritex",
-//            "-in", "src/test/resources/jatoo/cli/image/",
-//            "-out", "target/tests"
-//        });
-//    
-//    System.out.println();
-//    System.out.println("====================================================================================================");
-//    System.out.println();
-    
-//    new JatooCLI().execute(
-//        new String[] {
-//            "-image",
-//            "-rename",
-//            "-pattern", "yyyyMMdd-HHmmss",
-//            "-src", "c:\\Users\\cristian.sulea\\Downloads\\xxx\\20170507_082444585_iOS.jpg",
-//            "-dst", "c:\\Users\\cristian.sulea\\Downloads\\yyy"
-//        });
-    
-//    System.out.println();
-//    System.out.println("====================================================================================================");
-//    System.out.println();
-//    
-//    new JatooCLI().execute(
-//        new String[] {
-//            "-image",
-//            "-rename",
-//            "-pattern", "yyyyMMdd-HHmmss-${counter}",
-//            "-counterDigits", "5",
-//            "-src", "c:\\Users\\cristian.sulea\\Downloads\\xxx",
-//            "-dst", "c:\\Users\\cristian.sulea\\Downloads\\yyy"
-//        });
+  @Test
+  public void testResizeFit() throws Exception {
+
+    new JatooCLICommand().execute(
+      new String[] {
+          "-resize",
+          "-fit",
+          "-width", "400",
+          "-height", "200",
+          "-removeMetadata",
+          "-overwrite",
+          "-src", "src/test/resources/jatoo/cli/image/20141109144518.jpg",
+          "-dst", "target/tests/"
+      });
+
+    BufferedImage image = ImageUtils.read("target/tests/20141109144518.jpg");
+
+    Assert.assertTrue(image.getWidth() <= 400);
+    Assert.assertTrue(image.getHeight() <= 200);
+  }
+
+  @Test
+  public void testResizeFill() throws Exception {
+
+    new JatooCLICommand().execute(
+      new String[] {
+          "-resize",
+          "-fill",
+          "-width", "400",
+          "-height", "200",
+          "-removeMetadata",
+          "-overwrite",
+          "-src", "src/test/resources/jatoo/cli/image/20141109144518.jpg",
+          "-dst", "target/tests/"
+      });
+
+    BufferedImage image = ImageUtils.read("target/tests/20141109144518.jpg");
+
+    Assert.assertTrue(image.getWidth() == 400);
+    Assert.assertTrue(image.getHeight() == 200);
+  }
+
+  @Test
+  public void testResizeFolder() throws Exception {
+
+    new JatooCLICommand().execute(
+      new String[] {
+          "-resize",
+          "-fit",
+          "-width", "400",
+          "-height", "200",
+          "-removeMetadata",
+          "-overwrite",
+          "-src", "src/test/resources/jatoo/cli/image/",
+          "-dst", "target/tests/"
+      });
+
+    Assert.assertTrue(new File("src/test/resources/jatoo/cli/image/").list().length <= new File("target/tests/").list().length);
+  }
+
+  @Test
+  public void testRename1() throws Exception {
+
+    new JatooCLICommand().execute(
+      new String[] {
+          "-rename",
+          "-pattern", "yyMMddHHmmss",
+          "-src", "src/test/resources/jatoo/cli/image/20141109144518.jpg",
+          "-dst", "target/tests-rename1/"
+      });
+
+    Assert.assertTrue(new File("target/tests-rename1/").listFiles()[0].getName().length() == 16);
+  }
+
+  @Test
+  public void testRename2() throws Exception {
+
+    new JatooCLICommand().execute(
+      new String[] {
+          "-rename",
+          "-pattern", "yyyyMMdd-HHmmss-${counter}",
+          "-counterDigits", "5",
+          "-src", "src/test/resources/jatoo/cli/image/",
+          "-dst", "target/tests-rename2/"
+      });
+
+    Assert.assertTrue(new File("src/test/resources/jatoo/cli/image/").list().length <= new File("target/tests-rename2/").list().length);
   }
 
 }
